73482c26f2c0a1769377296f3508ebab5f0b48aa
dextercd <dexter.c.dopping@gmail.com>
2017-05-10 11:17:09 +0200

    Changed response system

--- include/twitch_chat_reader.hpp
+++ include/twitch_chat_reader.hpp
@@ -2,6 +2,8 @@
 #define TWITCH_CHAT_READER
 
 #include <string>
+#include <vector>
+#include <mutex>
 
 #include <asio.hpp>
 
@@ -14,10 +16,16 @@ public:
   void login(const std::string& user, const std::string& password, const std::string& channel);
 
   void do_read();
+  void do_read_completer(const asio::error_code& error, std::size_t bytes_transferred);
+
+  void send_responses();
+  void send_responses_completer(const asio::error_code& error, std::size_t bytes_transferred);
 
   void handle_message(const irc::message& twm);
 
   void ping_response(const std::string& server);
+
+  bool can_send();
 
   void close();
 
@@ -25,6 +33,9 @@ public:
   asio::ip::tcp::socket socket;
 
   asio::streambuf receive_buffer;
+
+  std::mutex responses_mutex;
+  std::vector<std::string> responses;
 };
 
 #endif
--- src/twitch_chat_reader.cpp
+++ src/twitch_chat_reader.cpp
@@ -12,64 +12,105 @@ using namespace std::literals;
 
 using asio::ip::tcp;
 
-client::client(asio::io_service& service, const std::string& user, const std::string& password, const std::string& channel)
+client::client(asio::io_service& service,
+               const std::string& user,
+               const std::string& password,
+               const std::string& channel)
   : io_service{&service}
   , socket{*io_service}
 {
   tcp::resolver resolver(*io_service);
   tcp::resolver::query query("irc.chat.twitch.tv", "6667");
-  
+
   asio::error_code connect_error;
   auto con = asio::connect(socket, resolver.resolve(query), connect_error);
 
-  if(!connect_error && con != tcp::resolver::iterator{}) {
-    std::cout << "Connected to " << con->host_name() << ':' << con->service_name() << ".\n";
-    login(user, password, channel);
-  } else {
-    std::cout << "Couldn't connect :(\n";
+  if(connect_error) {
+    std::cout << "Couldn't connect to the IRC server\n";
+    return;
   }
+
+  std::cout << "Connected to " << con->host_name() << ':' << con->service_name() << '\n';
+  login(user, password, channel);
 }
 
-// give login info
 void client::login(const std::string& user, const std::string& password, const std::string& channel)
 {
   asio::error_code error;
   asio::write(socket,
-              asio::buffer("PASS "s + password + "\r\nNICK " + user + "\r\nJOIN #"
-                           + channel
+              asio::buffer("PASS "s + password + "\r\nNICK " + user + "\r\nJOIN #" + channel
                            + "\r\nCAP REQ :twitch.tv/membership\r\nCAP REQ "
                              ":twitch.tv/commands\r\nCAP REQ "
                              ":twitch.tv/tags\r\n"),
               error);
+
   if(error) {
-    std::cout << "Error when sending login info: " << error.message() << ".\n";
-  } else {
-    std::cout << "Successfully send login info.\n";
-    do_read();
+    std::cout << "Error when sending login info: " << error.message() << '\n';
+    return;
   }
+
+  std::cout << "Successfully send login info.\n";
+  do_read();
 }
 
 void client::do_read()
 {
-  static auto do_read_impl = [this](const asio::error_code& ec, std::size_t byte_count) {
-    if(ec) {
-      return;
-    }
+  asio::async_read_until(socket,
+                         receive_buffer,
+                         "\r\n",
+                         std::bind(&client::do_read_completer, this, _1, _2));
+}
 
-    std::unique_ptr<char[]> message_data{new char[byte_count]};
-    receive_buffer.sgetn(message_data.get(), byte_count);
+void client::do_read_completer(const asio::error_code& error, std::size_t bytes_transferred)
+{
+  if(error) {
+    return;
+  }
 
-    // we copied the data, so we can start the next read now.
+  std::unique_ptr<char[]> message_data{new char[bytes_transferred]};
+
+  // copy the data into message_data
+  receive_buffer.sgetn(message_data.get(), bytes_transferred);
+
+  handle_message(irc::irc_message(message_data.get(), message_data.get() + bytes_transferred));
+
+  if(can_send()) {
+    send_responses();
+  } else {
     do_read();
+  }
+}
 
-    handle_message(irc::irc_message(message_data.get(), message_data.get() + byte_count));
-  };
+void client::send_responses()
+{
+  std::string response_data;
 
-  asio::async_read_until(socket, receive_buffer, "\r\n", do_read_impl);
+  std::unique_lock<std::mutex> lock{responses_mutex};
+  for(auto& r : responses) {
+    response_data += r;
+  }
+
+  responses.clear();
+  lock.unlock();
+
+  asio::async_write(socket,
+                    asio::buffer(response_data),
+                    std::bind(&client::send_responses_completer, this, _1, _2));
+}
+
+void client::send_responses_completer(const asio::error_code& error, std::size_t bytes_transferred)
+{
+  if(error) {
+    return;
+  }
+
+  // once we've send something we go back to reading
+  do_read();
 }
 
 void client::handle_message(const irc::message& irc_message)
 {
+  // log the message
   std::cout << irc_message << "\n\n";
 
   switch(irc_message.command) {
@@ -81,40 +122,21 @@ void client::handle_message(const irc::message& irc_message)
       // do nothing
     } break;
   }
-
-  /*
-  auto twm{twitch::twitch_message(irc_message)};
-  switch(twm.get_type()) {
-
-    case twitch::message_type::subscription: {
-      std::cout << twm.msubscription.display_name << " subscribed, for " <<
-  twm.msubscription.month_count << " months!";
-      if(!twm.msubscription.message.empty()) {
-        std::cout << " Message: " << twm.msubscription.message;
-      }
-      std::cout << '\n';
-    } break;
-
-    case twitch::message_type::chat: {
-      if(twm.mchat.bits >= 50) {
-        std::cout << "Thanks " << twm.mchat.display_name << " for the " << twm.mchat.bits << "
-  bits." << " Message: " << twm.mchat.message << '\n';
-      }
-    } break;
-
-    default: {
-      // do nothing
-    } break;
-  }*/
 }
 
 void client::ping_response(const std::string& server)
 {
-  asio::write(socket, asio::buffer("PONG :" + server + "\r\n"));
+  std::lock_guard<std::mutex> lock{responses_mutex};
+  responses.push_back("PONG :" + server + "\r\n");
 }
 
 void client::close()
 {
-  socket.shutdown(asio::socket_base::shutdown_type::shutdown_both);
   socket.close();
+}
+
+// TODO: track how many messages you've send to not get a timeout
+bool client::can_send() {
+  std::lock_guard<std::mutex> lock{responses_mutex};
+  return !responses.empty();
 }
